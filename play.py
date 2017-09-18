9b75264e1921c522e121199f11b5e171c2370f5a
Arnav Sivakumar <arnavk2007@gmail.com>
2017-09-17 20:34:00 -0500

    Add Marshawn image

--- play.py
+++ play.py
@@ -4,8 +4,17 @@ PLAY!!
 """
 import pygame
 pygame.init()
-pygame.display.set_mode((800, 600))
+WINDOW = pygame.display.set_mode((800, 600))
 pygame.display.set_caption('RRFOE')
+
+IMG_MARSHAWN = pygame.image.load('images/run-marshwn-run.png')
+IMG_MARSHAWN = pygame.transform.scale(IMG_MARSHAWN, (165, 256))
+def run_marshawn_run(pos_x, pos_y):
+    'run marshawn'
+    WINDOW.blit(IMG_MARSHAWN, (pos_x, pos_y))
+
+POS_X = (0)
+POS_Y = (0)
 
 CRASHED = False
 
@@ -13,5 +22,9 @@ while not CRASHED:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             CRASHED = True
+
+        WINDOW.fill((255, 255, 255))
+        run_marshawn_run(POS_X, POS_Y)
+
         print(event)
     pygame.display.update()
