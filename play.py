7d16c741708b06a3d81d6b84a2cc6c2afa5a735f
Arnav Sivakumar <arnavk2007@gmail.com>
2017-09-18 18:02:26 -0500

    Move Marshawn
    Assin keys to move Marshawn

--- play.py
+++ play.py
@@ -16,14 +16,38 @@ def run_marshawn_run(pos_x, pos_y):
 POS_X = (0)
 POS_Y = (0)
 
+CHANGE_X = 100
+CHANGE_Y = 0
+
 CRASHED = False
 
 while not CRASHED:
     for event in pygame.event.get():
+        WINDOW.fill((255, 255, 255))
         if event.type == pygame.QUIT:
             CRASHED = True
 
-        WINDOW.fill((255, 255, 255))
+        if event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_DOWN:
+                CHANGE_Y = 10
+            elif event.key == pygame.K_UP:
+                CHANGE_Y = -10
+            elif event.key == pygame.K_LEFT:
+                CHANGE_X = -10
+            elif event.key == pygame.K_RIGHT:
+                CHANGE_X = 10
+            
+        if event.type == pygame.KEYUP:
+            if event.key == pygame.K_DOWN or event.key == pygame.K_UP or event.key == pygame.K_RIGHT or event.key == pygame.K_LEFT:
+                CHANGE_X = 0 
+                CHANGE_Y = 0
+            
+        POS_X += CHANGE_X
+        # POS_Y += CHANGE_Y
+
+        if POS_X > 800:
+            POS_X = 800
+
         run_marshawn_run(POS_X, POS_Y)
 
         print(event)
